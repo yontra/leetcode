b150654ac545e554f1381dc48622ff8097b68a42
Mario <culjak.mario@gmail.com>
2025-01-13 21:27:49 +0100

    Medimu 2, add twon numbers

---
+++ m2/README.md
@@ -0,0 +1,12 @@
+Link: https://leetcode.com/problems/add-two-numbers/description/
+Name: Add two numbers
+Difficulty: Medium
+Description: 
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. 
+Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Submission: Accepted
+Runtime: 1ms, beats 100.00%
+Memory: 44.49MB, beats 63.3%
---
+++ m2/src/main/java/com/scadev/leetcode/LinkedList.java
@@ -0,0 +1,60 @@
+package com.scadev.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedList {
+
+    private ListNode head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public LinkedList(ListNode node) {
+        this.head = node;
+    }
+
+    public void push(int val) {
+        this.head = new ListNode(val, this.head);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof LinkedList other)) {
+            return false;
+        }
+
+        ListNode a = this.head;
+        ListNode b = other.head;
+
+        while (a != null && b != null) {
+            if (a.val != b.val) {
+                return false;
+            }
+
+            a = a.next;
+            b = b.next;
+        }
+
+        return (a == null && b == null);
+    }
+
+    @Override
+    public String toString() {
+        List<String> list = new ArrayList<>();
+
+        ListNode a = this.head;
+        while (a != null) {
+            list.add(String.valueOf(a.val));
+            a = a.next;
+        }
+
+        return String.join(",", list);
+    }
+
+}
