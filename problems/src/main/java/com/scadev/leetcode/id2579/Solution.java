0bfcb750fda3d0eceeb65b44fccdd7f6d5e6c6bd
Mario <culjak.mario@gmail.com>
2025-03-05 17:47:27 +0100

    Prepare id2579

---
+++ problems/src/main/java/com/scadev/leetcode/id2579/Solution.java
@@ -0,0 +1,9 @@
+package com.scadev.leetcode.id2579;
+
+class Solution {
+
+    public long coloredCells(int n) {
+        return 0;
+    }
+
+}
---
+++ problems/src/test/java/com/scadev/leetcode/id2579/SolutionTest.java
@@ -0,0 +1,35 @@
+package com.scadev.leetcode.id2579;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void coloredCells() {
+        List<Data> list = new ArrayList<>();
+        list.add(new Data(1, 1));
+        list.add(new Data(2, 5));
+
+        Solution s = new Solution();
+        for (Data data : list) {
+            long result = s.coloredCells(data.in);
+            var msg = String.format("in: %d, out: %d, actual: %d", data.in, data.out, result);
+            assertThat(result).withFailMessage(msg).isEqualTo(data.out);
+        }
+    }
+
+    static class Data {
+        int in;
+        long out;
+
+        Data(int in, long out) {
+            this.in = in;
+            this.out = out;
+        }
+    }
+}
