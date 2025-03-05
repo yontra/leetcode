b7124159bc0f490c85cfc6c43fa4671282d06a1c
Mario <culjak.mario@gmail.com>
2025-03-05 18:21:56 +0100

    Add comments

--- problems/src/main/java/com/scadev/leetcode/id2579/Solution.java
+++ problems/src/main/java/com/scadev/leetcode/id2579/Solution.java
@@ -7,16 +7,22 @@ class Solution {
             return 0;
         }
 
+        // size is column (and row) number of matrix that fits all tiles
         long size = (n * 2L) - 1;
+        // total is number of tiles in that matrix (rows * cols)
         long total = size * size;
 
+        // inverse calculation, calculate number of tiles that are NOT blue
+        // in one quadrant
         long q = 0;
         for (int i = 1; i <= n - 1; i++) {
             q = q + i;
         }
 
+        // there are 4 quadrants, so multiply that by 4
         long all = q * 4;
 
+        // subtract NOT blue tiles from all tiles give us number of blue tiles
         return total - all;
     }
 
