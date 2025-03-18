546cbddcdeb95815d93f492bfa76eb0f23379a8f
Mario <culjak.mario@gmail.com>
2025-03-18 22:43:32 +0100

    Add commont for sort and speed impact

--- problems/src/main/java/com/scadev/leetcode/id2206/Solution.java
+++ problems/src/main/java/com/scadev/leetcode/id2206/Solution.java
@@ -7,6 +7,8 @@ class Solution {
         }
 
         int n = nums.length;
+        // this is bubble sort
+        // Arrays.sort(nums) is faster, it uses tim sort
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (nums[i] < nums[j]) {
